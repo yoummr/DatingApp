b2f363591df1d8439f413115273d8d5b941cd625
yoummr <yoummr@gmail.com>
2023-07-20 22:57:17 -0400

    Login Parsistence

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +13,26 @@ export class AppComponent implements OnInit {
 
   users:any;
 
-  constructor(private http:HttpClient){
+  constructor(private http:HttpClient, private accountService: AccountService){
 
   }
   ngOnInit(): void {
+   this.getUser();
+   this.setCurrentUser();
+  }
+
+  getUser(){
     this.http.get('https://localhost:5001/api/Users').subscribe({
       next: (respose) => { this.users = respose },
       error: (error) => { console.log(error) },
       complete:()=>{ console.log('Request has completed') }
     });
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user : User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
