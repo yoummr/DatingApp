091baff483b3c0e692211bf68aaa9f50feb32be4
yoummr <yoummr@gmail.com>
2023-07-29 22:00:40 -0400

    Host Listener

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
 import { User } from 'src/app/_models/user';
 import { Member } from 'src/app/_modules/member';
@@ -12,10 +14,16 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberEditComponent implements OnInit {
 
+  @ViewChild('editForm') editForm : NgForm | undefined;
+  @HostListener('window:beforeunload',['$event']) unloadNotification($event:any){
+    if(this.editForm?.dirty){
+      $event.returnValue = true;
+    }
+  }
   member: Member | undefined;
   user: User | null = null;
 
-  constructor(private accountService:AccountService, private memberService: MembersService) {
+  constructor(private accountService:AccountService, private memberService: MembersService, private toastr: ToastrService) {
 
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next:(user)=> {
@@ -37,4 +45,10 @@ export class MemberEditComponent implements OnInit {
     });
   }
 
+  updateMember(){
+
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
+  }
 }
